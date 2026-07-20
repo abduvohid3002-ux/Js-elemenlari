9a55e08964d695e5ba2e9d9718b84e9d830c54c1
abduvohid3002-ux <abduvohid3002@gmail.com>
2026-07-20 09:19:31 +0500

    4-dars.js
    Added various function examples demonstrating function declaration, expression, and arrow functions in JavaScript.

---
+++ 4-dars.js
@@ -0,0 +1,99 @@
+Function
+
+// Umumiy ko'rinishi:
+// function functionName(parametr1, parametr2, ..., parametrn){
+//    statement 1;
+//    statement 2;
+//    statement 3;
+//    return output;
+// }
+
+// -------------------------------------
+
+// function meningFunksiyam(son1, son2, son3){ // function bu funsksiyani e'lon qilish uchun ishlatiladi 
+//     return son1 + son2 - son3;
+// } 
+
+// console. log(meningFunksiyam(20, 10, 5));
+// const result = meningFunksiyam(40, 5, 17);
+// console. log(result);
+
+// function hisoblashMashinasi(birinchiSon, ikkinchiSon, oxirgiSon) {
+//   return birinchiSon + ikkinchiSon - oxirgiSon;
+// }
+
+// let a=6, b=7, c=8;
+
+// console.log(hisoblashMashinasi(a,b))
+
+// -------------------------------------
+
+// Function decloration
+
+// const $ikkiningKvadrati = kvadrat(2);
+
+// function kvadrat(son1){
+//     return son1 * son1;
+// }
+
+// console.log($ikkiningKvadrati);
+
+
+// const $uchningKvadrati = kvadrat(3);
+
+// const $tortningKvadrati = kvadrat(4);
+
+// const $beshningKvadrati = kvadrat(5);
+
+// const $oltiningKvadrati = kvadrat(6);
+
+// const $yettiningKvadrati = kvadrat(7);
+
+// const $sakkizningKvadrati = kvadrat (8);
+
+// const $toqqizningKvadrati = kvadrat (9);
+
+// const $onningKvadrati = kvadrat (10);
+
+// console. log(`2 ning kvadrati ${$ikkiningKvadrati}
+// 3 ning kvadrati ${$uchningKvadrati}
+// 4 ning kvadrati ${$tortningKvadrati}
+// 5 ning kvadrati ${$beshningKvadrati}
+// 6 ning kvadrati ${$oltiningKvadrati}
+// 7 ning kvadrati ${$yettiningKvadrati}
+// 8 ning kvadrati ${$sakkizningKvadrati}
+// 9 ning kvadrati ${$toqqizningKvadrati}`)
+
+// -------------------------------------------
+
+// Function expression || Umumiy ko'rinishi:
+
+// const functionName = function(son1, son2, son3){
+//     // statement 1;
+//     // statement 2;
+//     // statement 3;
+//     // return output;
+// }
+
+// function kvadrat1(son1){
+//     return son1 * son1;
+// }
+
+// const kvadrat2 = function(son2){
+//     return son2 * son2;
+// }
+
+// console. log(kvadrat2 (10));
+
+// -------------------------------------------
+
+// Arrow function || Umumiy ko'rinishi:
+
+// const kvadrat = (son) => {
+//     return son * son;
+// }
+
+// let son = 10000;
+
+// const result = kvadrat(son);
+// console. log(result);
