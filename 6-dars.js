b9937250e203b14b24284f078e0178e04b73b0dc
abduvohid3002-ux <abduvohid3002@gmail.com>
2026-07-24 10:01:41 +0500

    6-dars.js

---
+++ 6-dars.js
@@ -0,0 +1,20 @@
+// find()
+
+// const findValue = (number) => number === 2;
+
+// const foundValue = result.find(findValue);
+
+// console.log(foundValue);
+
+// -------------------------------------------
+
+// const products = [
+//     {id: 3, product: "Iphone"},
+//     {id: 2, product: "Samsung"}
+// ];
+
+// const $findPhone = (product) => product.id === 3;
+// // const yordamida arrow function yaratildi, $findPhone deb nomlandi. product parametr berildi. product parametrining id si 1 teng bo'lsa
+// const $foundPhone = products.find($findPhone);
+// // const yordamida o'zgaruvchi yaratdik; $foundPhone deb nomlandi. products f
+// console. log($foundPhone);
