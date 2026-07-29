185715532e66d4d7c3ae96fdcaa3b8b8662128df
abduvohid3002-ux <abduvohid3002@gmail.com>
2026-07-29 09:42:43 +0500

    Comment out uniqueInOrder function and example call
    Comment out the entire uniqueInOrder function and its usage.

---
+++ 8-dars.js
@@ -0,0 +1,16 @@
+// const box = ('AAAABBBCCDAABB');
+
+// var uniqueInOrder = (iterable, mainResult) => {
+  
+//   const result = [...iterable];
+  
+//   for (let i = 0; i < result.length; i++) {
+//     if (result[i] === result[i + 1]) {
+//       delete result[i];
+//     }
+//   }
+
+//   return result;
+// }
+
+// console.log(uniqueInOrder(box));
