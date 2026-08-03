fc16f637130e9a155196603c29a0319b988cac57
abduvohid3002-ux <abduvohid3002@gmail.com>
2026-08-03 09:28:59 +0500

    Add initial implementation of map examples

---
+++ 9-dars.js
@@ -0,0 +1,26 @@
+
+// map()
+// const myFriends = ['n.akbarali', 'm.abubakr', 'a.saydullo', "a.ne'matullo"];
+        
+// const myFriendsNameLength = myFriends.map(
+//     function(myFriend) {
+//         return myFriend.length;
+//     }
+// )
+
+// console.log(myFriendsNameLength);
+
+// const chatMessanges = [
+//   {subjet: "user", message: "Salom!"},
+//   {subjet: "robot", message: "Salom, Sizga qanday yordam bera olaman?"},
+//   {subjet: "user", message: "Sen bilan suxbatlashsam bo'ladimi?"},
+//   {subjet: "robot", message: "Albatta, qaysi mavzu bo'yicha gaplashamiz?"}
+// ]
+
+// const messages = chatMessanges.map(
+//   function(a){
+//     return a.message;
+//   }
+// )
+
+// console.log(messages);
