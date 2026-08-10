b22ef094869c754aa3b35292d98334a8013487ee
abduvohid3002-ux <abduvohid3002@gmail.com>
2026-08-10 09:15:57 +0500

    Add examples for includes, sort, and conditionals

---
+++ 11-dars.js
@@ -0,0 +1,67 @@
+// includes()
+
+// const myFriends = ['n.akbarali', 'm.abubakr', 'a.saydullo', "a.ne'matullo"];
+
+// console. log (
+//     myFriends.includes(
+//         'n.akbarali', 'a.saydullo'                          // true
+//     )); 
+// console. log (
+//     myFriends.includes(
+//         'Salom'                                             // false
+//     )
+// )
+
+// -------------------------------------------------
+
+// sort()
+
+// const myFriends = [
+//     'n.akbarali',    // 11 ta belgi
+//     'm.abubakr',     // 9 ta belgi
+//     'a.saydullo',    // 10 ta belgi
+//     'a.ne\'matullo', // 12 ta belgi
+//     's.islom',       // 7 ta belgi
+//     'o.mirzohid',    // 10 ta belgi
+//     'j.xurshidbek',  // 12 ta belgi
+//     'b.aziz',        // 6 ta belgi
+//     'f.shaxzod',     // 9 ta belgi
+//     'z.javohir',     // 9 ta belgi
+//     'k.kamoliddin',  // 12 ta belgi
+//     'd.diyorbek',    // 10 ta belgi
+//     'u.umidjon',     // 9 ta belgi
+//     'h.hasan',       // 7 ta belgi
+//     'r.rustam'       // 8 ta belgi
+// ];
+
+// console.log(myFriends.sort());
+
+// -------------------------------------------------
+
+// const dasturchi = {
+//     ism: 'Abduvohid', 
+//     yosh: '22',
+//     salomlashish: function(){
+//         console. log("Salom!");
+//     }
+// };
+
+// console. log(Object.keys(dasturchi));
+// console. log(Object.values(dasturchi));
+// console. log(Object.entries(dasturchi));
+
+// -------------------------------------------------
+
+// If, else va else if
+
+// const number = "-2";
+// if (number >= 0) {
+//     console.log('Number is musbat');
+
+// } else if (number < 0) {
+//     console.log('Number is manfiy');
+// }
+
+// else{
+//     console.log(NaN)
+// };
