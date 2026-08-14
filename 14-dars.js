0d6fb06e8d31a5387090504341b1ffc7d66059b0
abduvohid3002-ux <abduvohid3002@gmail.com>
2026-08-14 09:03:24 +0500

    Add initial JavaScript code for DOM manipulation

---
+++ 14-dars.js
@@ -0,0 +1,12 @@
+// const myInput = document.querySelector('#first-name'); // inputni Jsga id orqali yukladik
+// const myButton = document.getElementById('findBtn'); // buttonni Js ga id orqali yukladik
+// const myHeading = document.getElementById('result'); // headingni Js ga id orqali yukladik
+// const container = document.querySelector('.container'); // divni Js ga class orqali yukladik
+
+// myHeading.textContent="Text";
+
+// const paragraph = document.createElement('p')
+// const text = document.createTextNode('Bu yerda text bor')
+// paragraph.appendChild(text)
+
+// container.append(paragraph);
