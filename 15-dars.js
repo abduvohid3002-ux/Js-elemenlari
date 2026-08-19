3a9177a0dca137942b2abaf93c02ac1a4f062b3e
abduvohid3002-ux <abduvohid3002@gmail.com>
2026-08-19 10:07:20 +0500

    Implement color change on button click
    Add color change functionality based on user input.

---
+++ 15-dars.js
@@ -0,0 +1,26 @@
+// const myInput = document.getElementById('first-name')
+// const myButton = document.getElementById('findBtn'); // buttonni Js ga id orqali yukladik
+// const body = document.body;
+
+// myButton.addEventListener('click', () => { 
+    
+//     const myColor = myInput.value.trim().toLowerCase();
+
+//     switch(myColor) {
+//         case 'red': body.style.cssText = `
+//             background-color: #ff0000;
+//         `
+//         break
+//         ;
+//         case 'yellow': body.style.cssText = `
+//             background-color: #e1ff00ff;
+//         `
+//         break
+//         ;
+//         case 'green': body.style.cssText = `
+//             background-color: #1aff00ff;
+//         `
+//         break
+//         ;
+//     }
+// });
